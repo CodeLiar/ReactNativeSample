4bfd773ef3ed2d93b36593c40bc692296defee47
Geass <mingjieding@linkdoc.com>
2016-06-30 14:18:02 +0800

    init

---
+++ ReactNativeSample/ios/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  ReactNativeSample
+//
+//  Created by 丁明杰 on 6/30/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
