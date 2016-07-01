dd8840717bce86295bc879774d4e706e02b15a83
Geass <mingjieding@linkdoc.com>
2016-07-01 14:02:37 +0800

    .a

---
+++ ReactNativeSample/ios/ReactLibrary/ReactLibrary.h
@@ -0,0 +1,13 @@
+//
+//  ReactLibrary.h
+//  ReactLibrary
+//
+//  Created by 丁明杰 on 7/1/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactLibrary : NSObject
+
+@end
---
+++ ReactNativeSample/ios/ReactLibrary/ReactLibrary.m
@@ -0,0 +1,13 @@
+//
+//  ReactLibrary.m
+//  ReactLibrary
+//
+//  Created by 丁明杰 on 7/1/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "ReactLibrary.h"
+
+@implementation ReactLibrary
+
+@end
