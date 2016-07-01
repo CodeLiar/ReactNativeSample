769f93d6387e9786c5fde963f88215a61d1cb7d0
Geass <mingjieding@linkdoc.com>
2016-07-01 11:53:58 +0800

    react framework

---
+++ ReactNativeSample/ios/ReactFramework/ReactFramework.h
@@ -0,0 +1,18 @@
+//
+//  ReactFramework.h
+//  ReactFramework
+//
+//  Created by 丁明杰 on 7/1/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactFramework.
+FOUNDATION_EXPORT double ReactFrameworkVersionNumber;
+
+//! Project version string for ReactFramework.
+FOUNDATION_EXPORT const unsigned char ReactFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactFramework/PublicHeader.h>
+#import <ReactFramework/MyViewController.h>
