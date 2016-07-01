0df8a8a5a602b59dc260b5a096946a02deaf8b57
Geass <mingjieding@linkdoc.com>
2016-07-01 10:39:02 +0800

    framework finish

---
+++ ReactNativeSample/ios/MyCarthageFramework/MyCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyCarthageFramework.h
+//  MyCarthageFramework
+//
+//  Created by 丁明杰 on 6/30/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCarthageFramework.
+FOUNDATION_EXPORT double MyCarthageFrameworkVersionNumber;
+
+//! Project version string for MyCarthageFramework.
+FOUNDATION_EXPORT const unsigned char MyCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCarthageFramework/PublicHeader.h>
+
+
